37de29819fbb805027f3be2df8e75adef5970c1d
Donatz03 <donadoni.mattia2003@gmail.com>
2023-03-27 17:58:57 +0200

    Added support for PNG files

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 #include "version.h"
 
 int main() {
-    std::cout << "Image viewer" << version << std::endl;
+    std::cout << "Image viewer - " << version << std::endl;
     BitmapImage b;
     std::string imgName = "test.png";
     if (b.load(imgName))
-        std::cout << "show image: " << imgName << std::endl;
+        std::cout << "Show image: " << imgName << std::endl;
     else
-        std::cerr << "image:" << imgName << " not readable." << std::endl;
+        std::cerr << "Image:" << imgName << " not readable." << std::endl;
     return 0;
 }
